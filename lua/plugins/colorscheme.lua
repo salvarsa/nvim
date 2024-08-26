a61d98d76670dd64255272ea8aa2fa5b768a391f
salvarsa <salvarsa.dev13@gmail.com>
2024-08-25 23:45:30 -0500

    feat: se agrega gitsingns

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,6 +1,6 @@
 return {
   {
-		'iruzo/matrix-nvim',
+    'iruzo/matrix-nvim',
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
@@ -9,4 +9,3 @@ return {
     end,
   }
 }
-
